b8de14809ed662c2694a878c0064e18037aa56fa
Ben Web <ben@benweb.com>
2018-10-08 16:53:43 +0100

    add all quests endpoint

--- controllers/quests.js
+++ controllers/quests.js
@@ -6,19 +6,21 @@ const
   Question = require('../models/Question');
 
 exports.getQuests = async (req, res, next) => {
-  const { venue_id } = req.params;
   const quests = await Quest.query()
     .skipUndefined()
-    .where('venue_id', '=', venue_id)
-    .orderBy('title');
   // need to look up how to handle errors here
   res.status(200).send({ quests });
 };
 
-/* exports.getQuestions = (req, res, next) => {
-  res.status(501).send({ message: 'Not Implemented' });
+exports.getQuestsByVenueId = async (req, res, next) => {
+  const { venue_id } = req.params;
+  const quests = await Quest.query()
+    .skipUndefined()
+    .where('venue_id', '=', venue_id)
+  // need to look up how to handle errors here
+  res.status(200).send({ quests });
 };
- */
+
 exports.getQuestions = async (req, res, next) => {
   const { quest_id } = req.params;
   const questions = await Question.query()
@@ -28,3 +30,7 @@ exports.getQuestions = async (req, res, next) => {
   res.status(200).send({ questions });
 };
 
+/* exports.getQuestions = (req, res, next) => {
+  res.status(501).send({ message: 'Not Implemented' });
+};
+ */
--- routes/quests.js
+++ routes/quests.js
@@ -4,11 +4,15 @@ const
   questsRouter = require('express').Router(),
   {
     getQuests,
+    getQuestsByVenueId,
     getQuestions
   } = require('../controllers/quests');
 
-questsRouter.route('/:venue_id')
+questsRouter.route('/')
   .get(getQuests);
+
+  questsRouter.route('/:venue_id')
+  .get(getQuestsByVenueId);
 
 questsRouter.route('/:quest_id/questions')
   .get(getQuestions);
