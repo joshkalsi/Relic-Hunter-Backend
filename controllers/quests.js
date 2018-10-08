acebd425d407e7c1d62d4a054728c2c27fc8c261
Ben Web <40058911+ben-web@users.noreply.github.com>
2018-10-08 11:33:53 +0100

    add route and model for venues and questions (#6)

--- controllers/quests.js
+++ controllers/quests.js
@@ -2,10 +2,10 @@
 
 const
   { transaction } = require('objection'),
-  Quest = require('../models/Quest');
+  Quest = require('../models/Quest'),
+  Question = require('../models/Question');
 
 exports.getQuests = async (req, res, next) => {
-
   const { venue_id } = req.params;
   const quests = await Quest.query()
     .skipUndefined()
@@ -15,6 +15,16 @@ exports.getQuests = async (req, res, next) => {
   res.status(200).send({ quests });
 };
 
-exports.getQuestions = (req, res, next) => {
+/* exports.getQuestions = (req, res, next) => {
   res.status(501).send({ message: 'Not Implemented' });
 };
+ */
+exports.getQuestions = async (req, res, next) => {
+  const { quest_id } = req.params;
+  const questions = await Question.query()
+    .skipUndefined()
+    .where('quest_id', '=', quest_id)
+  // need to look up how to handle errors here
+  res.status(200).send({ questions });
+};
+
---
+++ controllers/venues.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const
+  { transaction } = require('objection'),
+  Venue = require('../models/Venue');
+
+exports.getVenues = async (req, res, next) => {
+  const venues = await Venue.query()
+    .skipUndefined()
+  // need to look up how to handle errors here
+  res.status(200).send({ venues });
+};
+
