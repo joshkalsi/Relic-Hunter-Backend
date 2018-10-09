289cb465b048a079ac8c48516ea922ff65801734
joshkalsi <josh.kalsi@gmail.com>
2018-10-09 12:52:14 +0100

    Add try/catch blocks

--- controllers/quests.js
+++ controllers/quests.js
@@ -1,50 +1,76 @@
 'use strict';
 
 const
-  { transaction } = require('objection'),
+  // { transaction } = require('objection'),
   Quest = require('../models/Quest'),
   Question = require('../models/Question');
 
 exports.createQuest = async (req, res, next) => {
-  const { venue_id } = req.params;
-  const newQuest = { ...req.body.newQuest, venue_id };
-  const insertedNewQuest = await Quest.query()
-    .insert(newQuest);
-  res.status(201).send({ message: 'Quest Added', quest: insertedNewQuest });
+  try {
+    const { venue_id } = req.params;
+    const newQuest = { ...req.body.newQuest, venue_id };
+    const insertedNewQuest = await Quest.query()
+      .insert(newQuest);
+    res.status(201).send({ message: 'Quest Added', quest: insertedNewQuest });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.createQuestion = async (req, res, next) => {
-  const { quest_id } = req.params;
-  const { model_name, title, text, hint_text, answer_text } = req.body.newQuestion;
-  const newQuestion = { model_name, title, text, hint_text, answer_text, quest_id };
-  const insertedNewQuestion = await Question.query()
-    .insert(newQuestion);
-  res.status(201).send({ message: 'Question Added', question: insertedNewQuestion });
+  try {
+    const { quest_id } = req.params;
+    const { model_name, title, text, hint_text, answer_text } = req.body.newQuestion;
+    const newQuestion = { model_name, title, text, hint_text, answer_text, quest_id };
+    const insertedNewQuestion = await Question.query()
+      .insert(newQuestion);
+    res.status(201).send({ message: 'Question Added', question: insertedNewQuestion });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getQuests = async (req, res, next) => {
-  const quests = await Quest.query()
-    .skipUndefined();
-  // need to look up how to handle errors here
-  res.status(200).send({ quests });
+  try {
+    const quests = await Quest.query()
+      .skipUndefined();
+    // need to look up how to handle errors here
+    res.status(200).send({ quests });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getQuestsByVenueId = async (req, res, next) => {
-  const { venue_id } = req.params;
-  const quests = await Quest.query()
-    .where('venue_id', '=', venue_id);
+  try {
+    const { venue_id } = req.params;
+    const quests = await Quest.query()
+      .where('venue_id', '=', venue_id);
 
-  // test error handling
-  res.status(200).send({ quests });
+    // test error handling
+
+    /*   if (quests.length === 0) {
+        console.log('no quests');
+        throw createStatusCodeError(404);
+      } */
+
+    res.status(200).send({ quests });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getQuestions = async (req, res, next) => {
-  const { quest_id } = req.params;
-  const questions = await Question.query()
-    .skipUndefined()
-    .where('quest_id', '=', quest_id);
-  // need to look up how to handle errors here
-  res.status(200).send({ questions });
+  try {
+    const { quest_id } = req.params;
+    const questions = await Question.query()
+      .skipUndefined()
+      .where('quest_id', '=', quest_id);
+    // need to look up how to handle errors here
+    res.status(200).send({ questions });
+  } catch (err) {
+    next(err);
+  }
 };
 
 /* exports.getQuestions = (req, res, next) => {
@@ -52,3 +78,9 @@ exports.getQuestions = async (req, res, next) => {
 };
  */
 
+// The error returned by this function is handled in the error handler middleware in app.js.
+// function createStatusCodeError(statusCode) {
+//   return Object.assign(new Error(), {
+//     statusCode
+//   });
+// };
--- controllers/venues.js
+++ controllers/venues.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const
-  { transaction } = require('objection'),
+  // { transaction } = require('objection'),
   Venue = require('../models/Venue');
 
 exports.getVenues = async (req, res, next) => {
-  const venues = await Venue.query()
-    .skipUndefined();
-  // need to look up how to handle errors here
-  res.status(200).send({ venues });
+  try {
+    const venues = await Venue.query()
+      .skipUndefined();
+    // need to look up how to handle errors here
+    res.status(200).send({ venues });
+  } catch (err) {
+    next(err);
+  }
 };
